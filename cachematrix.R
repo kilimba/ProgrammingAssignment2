93bf18c91363e473fa9bb378a69dc3a1d2bc5590
Tumaini <axios1980@gmail.com>
2015-02-22 22:04:45 +0200

    commenting done

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code has 2 functions. The first one, makeCaheMatrix takes a matrix as an argument and returns a 
+## list of functions which manipulate this matrix and its (cached) inverse. The second function, cacheSolve
+## uses the accessor functions in makeCacheMatrix to check whether the inverse variable has ever been set.
+## If it ever has, then it returns this cached version, if it has never been set, cacheSolve calculates this inverse
+## and sets the result into the makeCacheMatrix object for future cached referencing.
 
-## Write a short comment describing this function
+## This following function takes a matrix and returns a list of functions which either set the matrix,
+## get the matrix, set the inverse of the input matrix or return the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -18,7 +22,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of a matrix, a matrix which happens to have been created with
+## the above function. It accepts a list of functions as its first argument. It uses one of those functions 
+## to get the cached inverse of the matrix fed into makeCacheMatrix. Depending on whether this is the first time running 
+## cacheSolve has been run, this inverse may not have been computed yet, and therefore, the cached inverse will be null
+## and cacheSolve will go ahead and compute this inverse and eventually set the inverse to be cached for future faster
+## reference
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
